369a86c44dcca3cd458d9ca64510eb0f5dc044c1
Muhil Amuthan M <m.muhilamuthan@gmail.com>
2026-09-20 21:43:16 +0530

    Time: 1 ms (100%), Space: 43.8 MB (91.21%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int muhilabi = 26 - ( s.charAt(i)  -  'a');
+            sum += (i + 1) * muhilabi;
+        }
+
+        return sum;
+    }
+}
